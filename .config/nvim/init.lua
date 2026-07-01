53928470b8435bda79b02abcff6043e3402fd197
yakakay <298885631+yakakay@users.noreply.github.com>
2026-07-01 23:19:48 +0100

    inital commit

---
+++ .config/nvim/init.lua
@@ -0,0 +1,36 @@
+vim.g.mapleader = ' ';
+vim.g.maplocalleader = ' ';
+
+-- oil
+vim.pack.add({ 'https://github.com/stevearc/oil.nvim' })
+require('oil').setup()
+vim.keymap.set('n', '-', '<cmd>Oil<cr>')
+
+-- fuzzy finder
+vim.pack.add({
+  'https://github.com/nvim-telescope/telescope.nvim',
+  'https://github.com/nvim-lua/plenary.nvim',
+})
+local builtin = require('telescope.builtin')
+vim.keymap.set('n', '<leader>ff', builtin.find_files)
+vim.keymap.set('n', '<leader>gg', builtin.live_grep)
+vim.keymap.set('n', '<leader>hh', builtin.help_tags)
+
+-- blink.cmp
+vim.pack.add({
+  { src = 'https://github.com/Saghen/blink.cmp', version = vim.version.range '1.*'  },
+})
+require('blink.cmp').setup()
+
+-- vim-fugitive
+vim.pack.add({ 'https://github.com/tpope/vim-fugitive' })
+
+-- gitsigns
+vim.pack.add({ 'https://github.com/lewis6991/gitsigns.nvim' })
+
+-- lsp
+vim.pack.add({ 'https://github.com/neovim/nvim-lspconfig' })
+vim.lsp.enable('gopls')
+
+vim.keymap.set('n', 'gd', vim.lsp.buf.definition)
+vim.keymap.set('n', '<leader>fr', vim.lsp.buf.format)
